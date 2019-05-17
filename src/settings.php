7b944a5e92c2ff89bf80428d351205adaedc419d
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-05-17 16:31:23 -0700

    add api and database settings to src/settings.php

--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,17 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        // API
+        'api' => [
+            'version' => 'v1',
+            'base_url' => 'http://localhost:8888',
+        ],
+
+        // Database
+        'db' => [
+            'dsn' => 'sqlite',
+            'database' => __DIR__.'/database/todo.db',
+        ],
     ],
 ];
