204cf2798ccab93305322bd2fac47853c3bcc7ba
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-05-17 16:46:00 -0700

    update src/dependencies.php -- formatting for readability

--- src/dependencies.php
+++ src/dependencies.php
@@ -3,36 +3,36 @@
 use Slim\App;
 
 return function (App $app) {
-    $container = $app->getContainer();
+  $container = $app->getContainer();
 
-    // view renderer
-    $container['renderer'] = function ($c) {
-        $settings = $c->get('settings')['renderer'];
-        return new \Slim\Views\PhpRenderer($settings['template_path']);
-    };
+  // view renderer
+  $container['renderer'] = function ($c) {
+    $settings = $c->get('settings')['renderer'];
+    return new \Slim\Views\PhpRenderer($settings['template_path']);
+  };
 
-    // monolog
-    $container['logger'] = function ($c) {
-        $settings = $c->get('settings')['logger'];
-        $logger = new \Monolog\Logger($settings['name']);
-        $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
-        $logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
-        return $logger;
-    };
+  // monolog
+  $container['logger'] = function ($c) {
+    $settings = $c->get('settings')['logger'];
+    $logger = new \Monolog\Logger($settings['name']);
+    $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
+    $logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
+    return $logger;
+  };
 
-    // api
-    $container['api'] = function($c) {
-        $api = $c->get('settings')['api'];
-        $api['api_url'] = $api['base_url'].'/api/'.$api['version'];
-        return $api;
-    };
+  // api
+  $container['api'] = function($c) {
+    $api = $c->get('settings')['api'];
+    $api['api_url'] = $api['base_url'].'/api/'.$api['version'];
+    return $api;
+  };
 
-      // db
-    $container['db'] = function($c) {
-        $db = $c->get('settings')['db'];
-        $pdo = new PDO($db['dsn'].':'.$db['database']);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        return $pdo;
-    };
+  // db
+  $container['db'] = function($c) {
+    $db = $c->get('settings')['db'];
+    $pdo = new PDO($db['dsn'].':'.$db['database']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+  };
 };
