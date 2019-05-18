5181957a9efa70e1c0e12ad74895d71c8488f1c4
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-05-17 21:25:31 -0700

    CRUD for Todo/Tasks

--- src/Model/Todo.php
+++ src/Model/Todo.php
@@ -20,16 +20,62 @@ class Todo {
     return $tasks;
   }
 
-  public function getTask($id){
-    $sql = 'SELECT * FROM tasks WHERE id = ? ORDER BY id';
-    $statement = $this->database->prepare($sql);
-    $statement->bindValue(1, $id, PDO::PARAM_INT);
+  public function getTask($task_id) {
+    $statement = $this->database->prepare(
+      'SELECT * FROM tasks WHERE id=:id'
+    );
+    $statement->bindParam('id', $task_id);
     $statement->execute();
     $task = $statement->fetch();
-
-    if (empty($task)) {
+    if (empty($tasks)) {
       throw new ApiException(ApiException::TASK_NOT_FOUND, 404);
     }
     return $task;
+  }
+
+  public function createTask($data) {
+    if (empty($data['title']) || empty($data['url'])) {
+      throw new ApiException(ApiException::TASK_INFO_REQUIRED);
+    }
+    $statement = $this->database->prepare(
+    'INSERT INTO tasks(title, url) VALUES(:title, :url)'
+    );
+    $statement->bindParam('title', $data['title']);
+    $statement->bindParam('url', $data['url']);
+    $statement->execute();
+    if ($statement->rowCount()<1) {
+      throw new ApiException(ApiException::TASK_CREATION_FAILED);
+    }
+    return $this->getTask($this->database->lastInsertId());
+  }
+
+  public function updateTask($data) {
+    if (empty($data['task_id']) || empty($data['title']) || empty($data['url'])) {
+      throw new ApiException(ApiException::TASK_INFO_REQUIRED);
+    }
+    $statement = $this->database->prepare(
+    'UPDATE tasks SET title=:title, url=:url WHERE id=:id'
+    );
+    $statement->bindParam('title', $data['title']);
+    $statement->bindParam('url', $data['url']);
+    $statement->bindParam('id', $data['task_id']);
+    $statement->execute();
+    if ($statement->rowCount()<1) {
+      throw new ApiException(ApiException::TASK_UPDATE_FAILED);
+    }
+    return $this->getTask($data['task_id']);
+  }
+
+  public function deleteTask($task_id) {
+    $this->getTask($task_id);
+    $statement = $this->database->prepare(
+      'DELETE FROM tasks WHERE id=:id'
+    );
+    $statement->bindParam('id', $task_id);
+    $statement->execute();
+    if ($statement->rowCount()<1) {
+      throw new ApiException(ApiException::TASK_DELETE_FAILED);
+    }
+    return ['message' => 'The task was deleted'];
   }
 }
