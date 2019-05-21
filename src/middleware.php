e6c13a0192e2ea3879458227c39b22021309901f
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-05-20 20:49:07 -0700

    update middleware by adding to CORS methods

--- src/middleware.php
+++ src/middleware.php
@@ -3,8 +3,15 @@
 use Slim\App;
 
 return function (App $app) {
+  $app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+  });
+
   $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
-    return $response->withHeader('Access-Control-Allow-Origin', '*');
+    return $response
+      ->withHeader('Access-Control-Allow-Origin', 'http://localhost')
+      ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+      ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   });
 };
